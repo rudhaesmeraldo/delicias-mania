5366fb94acf34136a46d9ce72cf45faa4c658420
rudhaesmeraldo <rudhaesmeraldo@gmail.com>
2025-02-11 17:26:03 -0300

    Ajustes de links. Criação do arquivos base. Criação de uma Partials.

--- receitas/views.py
+++ receitas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def receita(request):
+    return render(request,'receita.html')
